2bdc82e9e815a94840630f522aa6c42e17b0e02b
Lino Veloso <linoc.veloso@gmail.com>
2019-06-13 17:42:31 -0300

    implementing controllers, models, routers and some other things

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://dbAdmin:KHWjdk2QuhKGuyXJ@cluster0-ftmsm.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
 
+});
 app.get('/', (req, res) => {
-
-})
+    return res.send(`Olá amigo ${req.query.name}`);
+});
 
 
 app.listen(3333);
