2ad7fce1f9907986d6df1e445fc59453399b8791
Lino <linoc.veloso@gmail.com>
2019-06-16 22:49:24 -0300

    Produção de domingo

--- aulas/backend/src/controllers/PostController.js
+++ aulas/backend/src/controllers/PostController.js
@@ -1,13 +1,37 @@
 const Post = require('../models/Post');
-
+const sharp = require('sharp');
+const path = require ('path');
+const fs = require('fs');
 module.exports = {
     async index(req, res){
+        const posts = await Post.find().sort('-createdAt');
 
+        return res.json(posts);
     },
 
     async store(req, res){
-        console.log(req.body);
+        const {author, place, description,hashtags} = req.body;
+        const {filename: image} = req.file;
         
-        return res.json({ ok: true });
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
+
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70})
+            .toFile(
+                path.resolve(req.file.destination, 'resized', fileName)
+            )
+        fs.unlinkSync(req.file.path)    ;
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image: fileName,
+        });
+        return res.json({post});
     }
 };
+ 
--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const PostControllers = require('./controllers/PostController');
+const likeControllers = require('./controllers/LikeController');
+
 const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.get('/posts', PostControllers.index);
+routes.post('/posts', upload.single('image'), PostControllers.store);
 
 
-routes.post('/posts', PostControllers.store);
-
+routes.post('/posts/:id/like', likeControllers.store);
 //http://localhost:3333/posts
 module.exports = routes; 
